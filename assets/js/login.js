d67353c686417a4d70a89b8eb1dd948f232c96e0
aluno <Aluno@DEVE009914.educacional.up>
2022-04-19 22:06:14 -0300

    modificacoes na pagina de login, js atualizado e nomes css corrigido

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,5 +11,15 @@ function switchTab(nomeBotao){
         divRegister.style.display="block";
         divLogin.style.display="none";
     }
+}
 
+function recebeRegister(){
+    var password1=document.getElementById("inputConfirm");
+    var password2=document.getElementById("inputNewPassword");
+    
+    if(password1.value!=password2.value){
+        password1.style.color = "#FF0000";
+    }else{
+        password1.style.color = "#FFFFFF";
+    }
 }
