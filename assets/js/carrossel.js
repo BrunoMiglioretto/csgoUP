8adb4d867adafb316a9050d16d47250520cde49c
pauloponcz <paulo.ric03@gmail.com>
2022-04-20 17:30:21 -0300

    Feito ajustes no Arquivo style.css

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,16 +1,4 @@
 /*SCRIPT CARROSSEL*/
-const imgs = document.getElementsId("card");
-const img = document.querySelectorAll(".container-carrossel #figure");
-
-let idx =0;
-
-function carrossel(){
-    idx++;
-
-    if(idx>img.length-1){
-        idx =0;
-    }
-    imgs.style.transform = 'translateX($(-idx*200)px)';
-}
-
-setInterval(carrossel, 1800);
+var quant = document.querySelectorAll('.container-carrossel #card');
+var atual = 0;
+var imagem = document.getElementById
