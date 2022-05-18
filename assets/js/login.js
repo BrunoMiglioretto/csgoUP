e50e553ef62cf6d4f622ef1525328bd8479a438a
aluno <Aluno@DEVE009816.educacional.up>
2022-05-17 21:25:19 -0300

    atualizado as cores e algumas imagens do login

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,15 +2,24 @@
 function switchTab(nomeBotao){
     let divLogin=document.getElementById("login");
     let divRegister=document.getElementById("register");
+    let botaoLogin=document.getElementById("botaoLogin");
+    let botaoRegister=document.getElementById("botaoRegister");
+
 
     if(nomeBotao=="login"){
         divLogin.style.display="block";
         divRegister.style.display="none";
+        botaoLogin.style.color = "#00b94b";
+    }else{
+        botaoLogin.style.color = "#494949";
     }
 
     if(nomeBotao=="register"){
         divRegister.style.display="block";
         divLogin.style.display="none";
+        botaoRegister.style.color = "#00b94b";
+    }else{
+        botaoRegister.style.color = "#494949";
     }
 }
 
