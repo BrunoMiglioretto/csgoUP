cc739dacb8fdfd83e9b15079a82929145c42c744
bruno.miglioretto <bruno.miglioretto@obraprimaweb.com.br>
2022-06-18 16:48:04 -0300

    adicionar storage especifico para a trasacao

---
+++ assets/js/utils/Storages/transacaoStorage.js
@@ -0,0 +1,27 @@
+import { Storage } from "../storege.js";
+import { Transacao } from "../../modelos/transacao.js";
+
+export class TransacaoStorage extends Storage {
+    constructor() {
+        super("transacao");
+    }
+
+    buscarSorteiosSucesso() {
+        const listaTransacaoJSON = this.buscarLista();
+
+        const listaTransacao = [];
+        for (const item of listaTransacaoJSON) {
+            listaTransacao.push(new Transacao(
+                item.id,
+                item.tipo, 
+                item.deUsuario, 
+                item.paraUsuario, 
+                item.usuarioId, 
+                item.skinId, 
+                item.valor
+            ));
+        }
+
+        return listaTransacao.filter((item) => item.tipo == "ganhoRoleta");
+    }
+} 
